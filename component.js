0a2b5a102002f0174b072af2a5af513e332eaced
Richard Rodger <richard@ricebridge.com>
2017-12-15 22:55:46 +1100

    partial functioning

--- component.js
+++ component.js
@@ -5,7 +5,102 @@
 module.exports = function component() {
   var seneca = this
 
+  var map = {}
+  
   this.decorate('component', function component() {
-    return 'component'
+    var cmp = this.delegate()
+    cmp.component$ = {
+      id: cmp.did,
+    }
+    return cmp
   })
+
+
+  // TODO: Seneca should provide an API for this
+  this.private$.action_modifiers.push(function component_action_modifier(actdef) {
+    actdef.component = this.component$
+  })
+
+
+  this.inward(function(ctxt, data) {
+    var m = data.meta
+    var cmp = ctxt.seneca.component$ || {}
+    var actcmp = ctxt.actdef.component
+    
+    var msgspec = [
+      'M',
+      m.pattern,
+      m.id,
+      cmp.id || m.instance,
+      m.tag,
+      m.version,
+      m.sync ? 's' : 'a',
+      m.start,
+      m.parents.length,
+      m.pattern,
+      actcmp.id,
+      m.tag,
+      m.version,
+    ]
+
+    // TODO: needs to sample
+    update(map, msgspec)
+  })
+
+  
+  return {
+    export: {
+      map: map
+    }
+  }
+}
+
+
+// TODO: same as seneca-monitor; needs stadardising
+function update(map, data) {
+  var cmd = data[0]
+
+  var o = 1
+
+  var pattern = data[o + 0]
+
+  if (-1 != pattern.indexOf('role:seneca')) {
+    return
+  }
+  
+  var sync = data[o + 5]
+  var start = data[o + 6]
+  
+  var rid = data[o + 2]
+  var rtag = data[o + 3]
+  var rver = data[o + 4]
+  
+  var sid = data[o + 9]
+  var stag = data[o + 10]
+  var sver = data[o + 11]
+  
+  if ('' === pattern || rid === sid) {
+    return
+  }
+  
+  var r = (map[rid] = map[rid] || { r: {}, s: {}, t: rtag })
+  var s = (map[sid] = map[sid] || { r: {}, s: {}, t: stag })
+  
+  var rr = (r.r[pattern] = r.r[pattern] || {})
+  var sd = (rr[sid] = rr[sid] || {})
+  sd.t = stag
+  sd.v = sver
+  sd.s = sync
+  sd.c = 1 + (sd.c || 0)
+  sd.w = start
+  
+  var ss = (s.s[pattern] = s.s[pattern] || {})
+  var rd = (ss[rid] = ss[rid] || {})
+  rd.t = rtag
+  rd.v = rver
+  rd.s = sync
+  rd.c = 1 + (rd.c || 0)
+  rd.w = start
+  
+  r.last = s.last = start
 }
--- test/component.test.js
+++ test/component.test.js
@@ -21,3 +21,73 @@ lab.test('happy', fin => {
       fin()
     })
 })
+
+
+lab.test('id', fin => {
+  Seneca()
+    .test(fin)
+    .use('..')
+    .ready(function() {
+      var c0 = this.component()
+      var c1 = this.component()
+      var c01 = c0.component()
+
+      var c0_id = c0.component$.id
+      var c1_id = c1.component$.id
+      var c01_id = c01.component$.id
+
+      expect(c0_id).equal(c0.did)
+      expect(c1_id).equal(c1.did)
+      expect(c01_id).equal(c01.did)
+      expect(c01_id).startsWith(c0_id+'/')
+      
+      fin()
+    })
+})
+
+
+lab.test('id', fin => {
+  Seneca()
+    .test(fin)
+    .use('..')
+    .ready(function() {
+      var c0 = this.component()
+      c0.add('a:1')
+      var a1_def = c0.find('a:1')
+
+      expect(a1_def.component.id).equal(c0.component$.id)
+      
+      fin()
+    })
+})
+
+
+lab.test('simple-act', fin => {
+  Seneca()
+    .test(fin)
+    .use('..')
+    .ready(function() {
+      var x = 0
+      
+      var c0 = this.component()
+      c0.add('a:1', function a1(msg, reply) {
+        reply({x:++x})
+      })
+      
+      var c1 = this.component()
+
+      console.log('c0', c0.component$, 'c1', c1.component$)
+      
+      c1.act('a:1', function(ignore, out) {
+        console.log(out)
+        console.dir(this.export('component').map, {depth: null, colors: true})
+
+        c1.act('a:1', function(ignore, out) {
+          console.log(out)
+          console.dir(this.export('component').map, {depth: null, colors: true})
+          
+          fin()
+        })
+      })
+    })
+})
